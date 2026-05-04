05b504e27f464b7477054acdf06d5e9724f0ed32
dohaelsawy <dohaelsawy18@gmail.com>
2026-05-04 22:59:16 +0300

    update the endpoint for services

--- assessmentService/lms/src/main/java/com/lms/lms/Entity/QuizAttempt.java
+++ assessmentService/lms/src/main/java/com/lms/lms/Entity/QuizAttempt.java
@@ -1,6 +1,18 @@
 package com.lms.lms.Entity;
 
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
